b82a27ba6760bd1d4487c31d0c77e405bf0d0a74
Jakub Raciborski <raciborski.programista@gmail.com>
2021-11-30 20:49:07 +0100

    JQuery script choose time period is ready

---
+++ scripts/balanceSelectorChooseDate.js
@@ -0,0 +1,19 @@
+$("#timeInterval").change(function () {
+    var selectedOption = $("#timeInterval :selected").text();
+    if(selectedOption=="Choose dates") {
+		let selectON = '<div class="chooseDateTile">' +
+						'<div><label for="firstBalanceDay">First day:</label></div>' +
+						'<div><input id="firstBalanceDay" type="date" name="newEntryDate"></div>' +
+						'</div>' +
+						'<div class="chooseDateTile" id="lastDayBalanceChooseTile">' +
+						'<div><label for="lastBalanceDay">Last day:</label></div>' +
+						'<div><input id="lastBalanceDay" type="date" name="newEntryDate"></div>' +
+						'</div>' +
+						'<div class="clearBox"></div>';
+		$("#datesSelector").html(selectON);
+		}
+    else{
+			let selectOFF = ''
+			$("#datesSelector").html(selectOFF);
+		}
+});
